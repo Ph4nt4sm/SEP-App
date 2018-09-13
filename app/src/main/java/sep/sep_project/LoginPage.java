f25795f8011b905bfb9812bbe5a77eaae7c419e6
ChrisTaku <42629072+ChrisTaku@users.noreply.github.com>
2018-09-13 12:28:57 +1000

    Testing

--- app/src/main/java/sep/sep_project/LoginPage.java
+++ app/src/main/java/sep/sep_project/LoginPage.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -22,8 +23,20 @@ public class LoginPage extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-            }
-        });
-    }
+
+
+        };
+    });
 
 }
+    EditText username = (EditText)findViewById(R.id.username);
+    EditText password = (EditText)findViewById(R.id.PassWord);
+    public void login(View view) {
+        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+
+            //correct password
+        }else{
+            //wrong password
+        }
+    }
+    }
