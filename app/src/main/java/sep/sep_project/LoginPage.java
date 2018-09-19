df4329bd33e452336846732b291ec57f386e2d28
ChrisTaku <42629072+ChrisTaku@users.noreply.github.com>
2018-09-20 09:48:54 +1000

    Testing

--- app/src/main/java/sep/sep_project/LoginPage.java
+++ app/src/main/java/sep/sep_project/LoginPage.java
@@ -1,5 +1,6 @@
 package sep.sep_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -38,5 +39,10 @@ public class LoginPage extends AppCompatActivity {
         }else{
             //wrong password
         }
+    }
+
+    public void GoToRegister (View v){
+        Intent intent = new Intent( this, RegisterPage.class);
+        startActivity(intent);
     }
     }
--- app/src/main/java/sep/sep_project/RegisterPage.java
+++ app/src/main/java/sep/sep_project/RegisterPage.java
@@ -1,5 +1,6 @@
 package sep.sep_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,6 +25,10 @@ public class RegisterPage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void RegisterUser (View v){
+        Intent intent = new Intent( this, MainPage.class);
+        startActivity(intent);
     }
 
 }
