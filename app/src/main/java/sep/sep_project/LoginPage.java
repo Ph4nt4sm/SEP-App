6a25e28c4efea78b5aa7287702fbc8dcb1267a94
ChrisTaku <42629072+ChrisTaku@users.noreply.github.com>
2018-10-04 11:20:47 +1000

    Push Login, Register, Recover pages

--- app/src/main/java/sep/sep_project/LoginPage.java
+++ app/src/main/java/sep/sep_project/LoginPage.java
@@ -65,39 +65,7 @@ public class LoginPage extends AppCompatActivity {
     }
     // [END on_start_check_user]
 
-    private void createAccount(String email, String password) {
-        Log.d(TAG, "createAccount:" + email);
-        if (!validateForm()) {
-            return;
-        }
 
-        showProgressDialog();
-
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(LoginPage.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-                        // [START_EXCLUDE]
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
-                    }
-                });
-        // [END create_user_with_email]
-    }
 
     private void signIn(String email, String password) {
         Log.d(TAG, "signIn:" + email);
@@ -154,7 +122,7 @@ public class LoginPage extends AppCompatActivity {
                     public void onComplete(@NonNull Task<Void> task) {
                         // [START_EXCLUDE]
                         // Re-enable button
-                        findViewById(R.id.verify_email_button).setEnabled(true);
+                        findViewById(R.id.login).setEnabled(true);
 
                         if (task.isSuccessful()) {
                             Toast.makeText(LoginPage.this,
@@ -223,8 +191,6 @@ public class LoginPage extends AppCompatActivity {
             createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
         } else if (i == R.id.GoToRegister) {
             signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-        } else if (i == R.id.sign_out_button) {
-            signOut();
         } else if (i == R.id.verify_email_button) {
             sendEmailVerification();
         }
--- app/src/main/java/sep/sep_project/RegisterPage.java
+++ app/src/main/java/sep/sep_project/RegisterPage.java
@@ -47,16 +47,41 @@ public class RegisterPage extends AppCompatActivity {
             }
         });
         mAuth = FirebaseAuth.getInstance();
+        createAccount();
     }
 
+        private void createAccount(String email, String password) {
+            Log.d(TAG, "createAccount:" + email);
+            if (!validateForm()) {
+                return;
+            }
 
-    private void createAccount(String email, String password) {
-        Log.d(TAG, "createAccount:" + email);
-        if (!validateForm()) {
-            return;
-        }
+            showProgressDialog();
 
-        showProgressDialog();
+            // [START create_user_with_email]
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(LoginPage.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+
+                            // [START_EXCLUDE]
+                            hideProgressDialog();
+                            // [END_EXCLUDE]
+                        }
+                    });
+            // [END create_user_with_email]
 
         // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
