5af9f3c94b3b603f689f2decdb28d3f06f877ee8
Ph4nt4sm <declan.l@outlook.com>
2018-10-21 14:36:23 +1100

    Database integration
    added database integration

--- app/src/main/java/sep/sep_project/MainPage.java
+++ app/src/main/java/sep/sep_project/MainPage.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.TextView;
 
 
 public class MainPage extends AppCompatActivity {
@@ -13,6 +13,12 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        getDatabaseData database = new getDatabaseData();
+
+
+        final TextView textViewToChange = (TextView) findViewById(R.id.testText);
+        new GetUrlContentTask(textViewToChange).execute("http://124.171.219.142/index.php?get_restaraunts");
 
     }
     public void GoTo (View v){
---
+++ app/src/main/java/sep/sep_project/getDatabaseData.java
@@ -0,0 +1,85 @@
+package sep.sep_project;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import android.os.AsyncTask;
+import android.widget.TextView;
+import com.google.gson.*;
+
+public class getDatabaseData{
+    public int id;
+    public int postcode;
+    public String address;
+    public String state;
+    public String category;
+    public String details;
+    public String image_location;
+
+}
+
+
+class GetUrlContentTask extends AsyncTask<String, Integer, String> {
+    public String output = null;
+    public TextView mTextView = null;
+    GetUrlContentTask(TextView tv){
+        mTextView = tv;
+    }
+    protected String doInBackground(String... urls) {
+        URL url = null;
+        try {
+            url = new URL(urls[0]);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        connection.setDoOutput(true);
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+        try {
+            connection.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader rd = null;
+        try {
+            rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String content = "", line;
+        try {
+        while ((line = rd.readLine()) != null) {
+            content += line + "\n";
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return content;
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+    }
+
+    protected void onPostExecute(String result) {
+
+        Gson gson = new Gson();
+        getDatabaseData[] databaseRes = gson.fromJson(result, getDatabaseData[].class);
+        mTextView.setText(databaseRes[0].address);
+    }
+}
+
+
