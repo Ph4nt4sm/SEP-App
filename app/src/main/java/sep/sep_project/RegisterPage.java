b59aebff3bb602cac62638ffada7948b196c85a0
ChrisTaku <42629072+ChrisTaku@users.noreply.github.com>
2018-10-25 09:47:52 +1100

    Not Using FireBase

--- app/src/main/java/sep/sep_project/RegisterPage.java
+++ app/src/main/java/sep/sep_project/RegisterPage.java
@@ -1,7 +1,9 @@
 package sep.sep_project;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +40,42 @@ public class RegisterPage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    EditText username = (EditText) findViewById(R.id.registerEmail);
+    EditText password = (EditText) findViewById(R.id.registerPassword);
+    Button b1 = (Button)findViewById(R.id.RegisterBtn);
+
+    public void login(View view) {
+        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            //correct password
+        } else {
+            //wrong password
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+            username.setVisibility(View.VISIBLE);
+            username.setBackgroundColor(Color.RED);
+            int counter = 3;
+            counter--;
+            username.setText(Integer.toString(counter));
+
+            if(counter==0){
+                //disables the button, sets timer to 60 seconds before the button reactivates
+                new CountDownTimer(60000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        Toast.makeText(getApplicationContext(), "Try again in 60 seconds",Toast.LENGTH_SHORT).show();
+                        username.setVisibility(View.VISIBLE);
+                        username.setBackgroundColor(Color.RED);
+                        b1.setEnabled(false);
+                    }
+
+                    public void onFinish() {
+                        Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+                        b1.setEnabled(true);
+                    }
+                }.start();
+            }
+        }
     }
 
 }
