3e9e6e63d97020d5bb04fcf5a015c5df9d93c4b2
Ph4nt4sm <declan.l@outlook.com>
2018-10-25 22:13:01 +1100

    removed google_service stuff (firebase leftovers)

--- app/src/main/java/sep/sep_project/RecoverUserDetails.java
+++ app/src/main/java/sep/sep_project/RecoverUserDetails.java
@@ -11,9 +11,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.security.PublicKey;
 
--- app/src/main/java/sep/sep_project/RegisterPage.java
+++ app/src/main/java/sep/sep_project/RegisterPage.java
@@ -17,11 +17,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterPage extends AppCompatActivity {
 
